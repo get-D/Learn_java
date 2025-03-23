b3f9cc1a8a0b9902ececcfe3e7f6490ac38f7e48
get-D <divyatwa5@gmail.com>
2025-03-24 02:53:49 +0530

    printing variables

--- variables.java
+++ variables.java
@@ -2,6 +2,17 @@ import java.lang.*;
 
 class Variables{
 	public static void main(string[]args){
-	
+
+		byte b=5;
+	        short s=300;
+        	int i=100;
+	        float f=25.3f;
+	        char c='A';
+        
+        	System.out.println(b);
+	        System.out.println(s);
+        	System.out.println(i);
+	        System.out.println(f);
+        	System.out.println(c);
 	}
 }
