b12ad62010f93ee28fd87f6e876d5568ba4729bc
get-D <divyatwa5@gmail.com>
2025-03-24 02:33:34 +0530

    Data size and range of float

--- DataSizeRange.java
+++ DataSizeRange.java
@@ -2,6 +2,8 @@ import java.lang.*;
 
 class DataSizeRange{
 	public static void main(string[]args){
-		
+		System.out.println("FLoat Min: "+Float.MIN_VALUE);
+        	System.out.println("Float Max: "+Float.MAX_VALUE);
+        	System.out.println("Float Bytes :"+Float.BYTES);
 	}
 }
